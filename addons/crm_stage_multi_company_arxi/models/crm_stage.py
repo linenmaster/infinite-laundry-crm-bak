c3a02cd95e7d061ca44e6b3921b7718ccdddb43a
Sanket Modi <sanket.msc9@gmail.com>
2025-08-18 16:53:01 +0530

    [FIX] crm automation >> multi-company issue on creating a new pipeline

--- addons/crm_stage_multi_company_arxi/models/crm_stage.py
+++ addons/crm_stage_multi_company_arxi/models/crm_stage.py
@@ -16,3 +16,11 @@ class CrmStage(models.Model):
         default=lambda self:
             self.env["res.company"]._company_default_get("crm.stage"),
     )
+
+class CrmLead(models.Model):
+    _inherit = "crm.lead"
+    def _stage_find(self, team_id=False, domain=None, order='sequence, id', limit=1):
+        """ override the function """
+        res = super(CrmLead, self)._stage_find(team_id=team_id, domain=domain, order=order, limit=limit)
+        domain += [('company_ids','in',[self.env.user.company_id.id])]
+        return self.env['crm.stage'].search(domain, order=order, limit=limit)
